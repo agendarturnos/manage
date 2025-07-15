3cc43d88b716af897c8337d7cf0b00aade78a454
agendarturnos <agendarturnosonline@gmail.com>
2025-07-14 23:37:09 -0300

    Apply mockup styles to React app

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// base styles from the original mockup
+import './mockup.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
